bf1368559f43c28852241d081912de6902ac227a
Yevhenii <verhovich@gmail.com>
2019-02-18 17:30:59 +0200

    3nd commit (Regexp processing...)

--- src/regExp/controller/RegExpContainer.java
+++ src/regExp/controller/RegExpContainer.java
@@ -1,19 +1,18 @@
 package regExp.controller;
-import java.util.regex.Pattern;
 
 interface RegExpContainer {
 
-    String patternSubscriberSurname                = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}([-|\\s]{1}[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?"; //^[А-ЯҐІЇЄ][а-яґіїє'-\s]{2,20}$
-    String patternSubscriberName                   = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}([-|\\s]{1}[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?";
-    String patternSubscriberMiddleName             = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}([-|\\s]{1}[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?";
-    String patternSubscriberNickname               = "[A-Za-z0-9-.]{1,30}";
-    String patternSubscriberComments               = ".{0,100}";
+    String patternSubscriberSurname                = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}((-|\\s){1}[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?";
+    String patternSubscriberName                   = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}((-|\\s){1}[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?";
+    String patternSubscriberMiddleName             = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}((-|\\s){1}[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?";
+    String patternSubscriberNickname               = "[A-Za-z0-9-\\.]{1,30}";
+    String patternSubscriberComments               = "[^\\n](.){0,100}";
 //  String patternSubscriberGroups                 = "";
     String patternSubscriberHomePhone              = "\\d{3}-\\d{3}-\\d\\d-\\d\\d";
     String patternSubscriberCellPhone_1            = "\\d{3}-\\d{3}-\\d\\d-\\d\\d";
     String patternSubscriberCellPhone_2            = "(\\d{3}-\\d{3}-\\d\\d-\\d\\d)??";
-    String patternSubscriberEmail                  = "[A-Za-z0-9-.]{1,50}@[A-Za-z0-9-.]{1,50}.([A-Za-z]){2,4}";
-    String patternSubscriberSkype                  = "[A-Za-z0-9-.]{1,30}";
+    String patternSubscriberEmail                  = "[A-Za-z0-9-\\.]{1,50}@[A-Za-z0-9-.]{1,50}.([A-Za-z]){2,4}";
+    String patternSubscriberSkype                  = "[A-Za-z0-9-\\.]{1,30}";
     String patternSubscriberZipCode                = "\\d{5,7}";
     String patternSubscriberCity                   = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}+(-[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?";
     String patternSubscriberStreet                 = "^[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20}+([\\s-]?[А-ЯҐІЇЄ]{1}[а-яґіїє']{1,20})?";
